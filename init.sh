c42a029d74af91caa659dc3b5144b83f5e2b029f
Ryan Othniel Kearns <ryankearns25@gmail.com>
2021-04-12 21:36:43 -0700

    Add script for vim plugins

---
+++ init.sh
@@ -0,0 +1,8 @@
+# lightline
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+
+# gitgutter
+mkdir -p ~/.vim/pack/airblade/start
+cd ~/.vim/pack/airblade/start
+git clone https://github.com/airblade/vim-gitgutter.git
+vim -u NONE -c "helptags vim-gitgutter/doc" -c q
